e2e291c6426fe71b04bf11176804508ccab0993d
Touhidulislam622 <106024626+Touhidulislam622@users.noreply.github.com>
2023-02-04 01:35:28 +0600

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,35 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+import  {reducer}  from "./Reducer";
 
-const bookData = [
-  { id: 1, name: "A place called Home" },
-  { id: 2, name: "Living with the Dead" },
-  { id: 3, name: "To kill a Mockingbird" },
+const booksData = [
+  { id: 1, name: "Pather Panchal" },
+  { id: 2, name: "Padma Nadir Majhi" },
+  { id: 3, name: "  Srikanta" },
 ];
 
-const Modal = ({modalText}) => {
-  return <p>{modalText}</p>
+const Modal = ({ modalText }) => {
+  return <p>{modalText}</p>;
 };
 
+const initialState = {
+  books: booksData,
+  isModalOpen: false,
+  modalText: "",
+};
 const UseReducer = () => {
-  const [books, setBooks] = useState(bookData);
-  const [isModalOpen , setIsModalOpen] = useState(false);
-  const [modalText , setModalText] = useState("");
+  const [bookState, dispatch] = useReducer(reducer, initialState);
   const [bookName, setBookName] = useState("");
 
- const handleSubmit = (e) => {
-  e.preventDefault();
-  setBooks((prevState) => {
-    const newBook = {id: new Date().getTime().toString, name: bookName };
-    return [...prevState , newBook ]
-  });
-  setIsModalOpen(true);
-  setModalText("Book is added By UuH")
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = { id: new Date().getTime().toString(), name: bookName };
+    dispatch({ type: "ADD", payload: newBook });
+    setBookName("");
+  };
 
+  const removeBook = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
   return (
     <div>
       <h3>Book List</h3>
-
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -38,16 +41,25 @@ const UseReducer = () => {
             setBookName(e.target.value);
           }}
         />
-        <button type="submit">Submit</button>
+        <button type="submit">Add Book</button>
       </form>
 
-      { isModalOpen && <Modal modalText ={modalText} 
-       />}
+      {bookState.isModalOpen && <Modal modalText={bookState.modalText} />}
 
-      {books.map((book) => {
+      {bookState.books.map((book) => {
         const { id, name } = book;
-
-        return <li key={id}> {name} </li>;
+        return (
+          <li key={id}>
+            {name}{" "}
+            <button
+              onClick={() => {
+                removeBook(id);
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        );
       })}
     </div>
   );
